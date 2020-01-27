069fdf15df5e30a61642e5098b9df91d7f5ac07a
Pratheep <theep1891@gmail.com>
2020-01-27 19:38:18 +0100

    Section-Slider added

--- src/components/Pages.vue
+++ src/components/Pages.vue
@@ -1,5 +1,13 @@
 <template>
 <div>
+    <div class="section-slider">
+    <ul>
+        <li><a href="#Profile">01</a></li>
+        <li><a href="#Skills">02</a></li>
+        <li><a href="#Portfolio">03</a></li>
+        <li><a href="#Contact">04</a></li>
+    </ul>
+</div>
   <div id="Profile" class="section">
         <div class="text-wr">
             <h1 class="title">
