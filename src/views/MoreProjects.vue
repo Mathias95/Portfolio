ffe0ce68774145523ea578756e48f61c89814199
Pratheep <theep1891@gmail.com>
2020-02-21 10:44:09 +0100

    more project update soon added

--- src/views/MoreProjects.vue
+++ src/views/MoreProjects.vue
@@ -40,6 +40,9 @@
   <br>
   <br>
 </div> -->
+ <h1>
+  UPDATE SOON ......
+</h1>
  <div class="progress">
   <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div>
  </div>
