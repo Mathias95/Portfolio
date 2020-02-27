858972d7cc4f10b9d3f4f998965371b430465372
Mathias Jacobsen <mathiasrjacobsen@gmail.com>
2020-02-27 13:03:25 +0100

    fixed styling and html structure

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,77 +1,145 @@
 <template>
-   <div>
-    <b-container id="Contact">
-   <b-row>
-    <b-col cols="12" md="8" class="contactform">
-        <form action="POST" data-netlify="true" name="contact" class="needs-validation" novalidate>
-    <div>
-    <b-form-input class="col-xs-4" type="text" placeholder="First Name" name="firstname" value="" required></b-form-input>
-    <div class="invalid-feedback">
-        Please type a First Name
-    </div>
-    <div class="mt-2"></div>
-  </div>
-   <div>
-    <b-form-input  type="text" placeholder="Last Name" name="lastname" value="" required></b-form-input>
-    <div class="invalid-feedback">
-        Please type a Last Name
-    </div>
-    <div class="mt-2"></div>
-  </div>
-   <div>
-    <b-form-input placeholder="E-mail" type="email" value="" name="email" required></b-form-input>
-     <div class="invalid-feedback">
-        Please type a valid e-mail address
-      </div>
-    <div class="mt-2"></div>
-  </div>
   <div>
-  <b-form-textarea id="textarea-rows" placeholder="Message" name="message" rows="8" value="" required></b-form-textarea>
-  <div class="invalid-feedback">
-        Please type a Message
-    </div>
-   <div class="mt-2"></div>
-</div>
-<div>
-  <b-button variant="danger" type="submit">Send</b-button>
-</div>
- </form>
-    </b-col>
-   <!-- Break contact form and contact icon -->
-    <b-col cols="6" md="4" class="contacticons">
-        <b-row cols="2">
-    <b-col md="4"> <a href="https://www.facebook.com/theep1891" target="_blank"><img class="ctimg-responsive" src="../assets/img/contact/facebook.gif" alt="" /></a></b-col><br>
-    <b-col md="4"><a class="LI-simple-link" href='https://dk.linkedin.com/in/pratheep-dk?trk=profile-badge' target="_blank"><img class="ctimg-responsive" src="../assets/img/contact/linkedin.gif" alt="" /></a></b-col>
-    </b-row>
-    <br>
-    <b-row cols="2">
-    <b-col md="4"><a href="mailto:theep1891@gmail.com"><img class="ctimg-responsive" src="../assets/img/contact/mail.gif" alt="" /></a></b-col>
-    <b-col md="4"><a href="tel:+45 71170470"><img class="ctimg-responsive" src="../assets/img/contact/viber.gif" alt="" /></a></b-col>
-    </b-row>
-    </b-col>
-  </b-row>
-  </b-container>
-   </div>
- </template>
+    <b-container id="Contact">
+      <b-row class="justify-content-center">
+        <b-col cols="12" md="8" class="contactform">
+          <form
+            action="POST"
+            data-netlify="true"
+            name="contact"
+            class="needs-validation"
+            novalidate
+          >
+            <div>
+              <b-form-input
+                class="col-xs-4"
+                type="text"
+                placeholder="First Name"
+                name="firstname"
+                value=""
+                required
+              ></b-form-input>
+              <div class="invalid-feedback">
+                Please type a First Name
+              </div>
+              <div class="mt-2"></div>
+            </div>
+            <div>
+              <b-form-input
+                type="text"
+                placeholder="Last Name"
+                name="lastname"
+                value=""
+                required
+              ></b-form-input>
+              <div class="invalid-feedback">
+                Please type a Last Name
+              </div>
+              <div class="mt-2"></div>
+            </div>
+            <div>
+              <b-form-input
+                placeholder="E-mail"
+                type="email"
+                value=""
+                name="email"
+                required
+              ></b-form-input>
+              <div class="invalid-feedback">
+                Please type a valid e-mail address
+              </div>
+              <div class="mt-2"></div>
+            </div>
+            <div>
+              <b-form-textarea
+                id="textarea-rows"
+                placeholder="Message"
+                name="message"
+                rows="8"
+                value=""
+                required
+              ></b-form-textarea>
+              <div class="invalid-feedback">
+                Please type a Message
+              </div>
+              <div class="mt-2"></div>
+            </div>
+            <div class="form-button">
+              <b-button variant="danger" type="submit">Send</b-button>
+            </div>
+          </form>
+        </b-col>
+        <!-- Break contact form and contact icon -->
+        <b-col cols="12" md="4" class="contacticons">
+					<div class="icon-container">
+						<div class="icon-wrapper">
+							<a href="https://www.facebook.com/theep1891" target="_blank"
+								><img
+									class="ctimg-responsive"
+									src="../assets/img/contact/facebook.gif"
+									alt=""
+							/></a>
+						</div>
+						<div class="icon-wrapper">
+							<a
+								class="LI-simple-link"
+								href="https://dk.linkedin.com/in/pratheep-dk?trk=profile-badge"
+								target="_blank"
+								><img
+									class="ctimg-responsive"
+									src="../assets/img/contact/linkedin.gif"
+									alt=""
+							/></a>
+						</div>
+						<div class="icon-wrapper">
+							<a href="mailto:theep1891@gmail.com"
+								><img
+									class="ctimg-responsive"
+									src="../assets/img/contact/mail.gif"
+									alt=""
+							/></a>
+						</div>
+						<div class="icon-wrapper">
+							<a href="tel:+45 71170470"
+								><img
+									class="ctimg-responsive"
+									src="../assets/img/contact/viber.gif"
+									alt=""
+							/></a>
+						</div>
+					</div>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
 
 <script>
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (function () {
   'use strict'
-  window.addEventListener('load', function () {
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.getElementsByClassName('needs-validation')
-    // Loop over them and prevent submission
-    // eslint-disable-next-line no-unused-vars
-    var validation = Array.prototype.filter.call(forms, function (form) {
-      form.addEventListener('submit', function (event) {
-        if (form.checkValidity() === false) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
-        form.classList.add('was-validated')
-      }, false)
-    })
-  }, false)
+  window.addEventListener(
+    'load',
+    function () {
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var forms = document.getElementsByClassName('needs-validation')
+      // Loop over them and prevent submission
+      // eslint-disable-next-line no-unused-vars
+      var validation = Array.prototype.filter.call(forms, function (form) {
+        form.addEventListener(
+          'submit',
+          function (event) {
+            if (form.checkValidity() === false) {
+              event.preventDefault()
+              event.stopPropagation()
+            }
+            form.classList.add('was-validated')
+          },
+          false
+        )
+      })
+    },
+    false
+  )
 })()
 </script>
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,49 +1,30 @@
 <template>
-<div class="animated bounceInLeft delay-4s">
-  <div class="Profile" >
-    <!-- <h1>This is an profile page</h1> -->
-    <!-- <div class="p-details">
-      <heading>Completed Computer Science and BA top-up in Web Development at Erhvervsakademi og Professionshøjskole (UCL) in Odense.</heading>
-      <p>C# (OOP), Asp.net MVC, CMS (Umbraco, Strapi), Xamarin, Scrum, Git, SQL, System Development / Modelling, Interface Design-UX, Html, CSS, Vue.js, Nuxt.js, WordPress, JavaScript </p>
-    </div>
-    <div class="p-image">
-      <img src="../assets/img/theeplogo.jpg" alt="">
-    </div> -->
-
-    <!-- <div class="col-lg-6 col-md-4 col-sm-6 col-xs-12">
-      <div class="p-details">
-        <h4>
-          Hi! My name is Pratheep Sinnappu. I have Completed Computer Science and BA top-up in Web Development at
-          Erhvervsakademi og Professionshøjskole (UCL) in Odense.<br>
-          </h4>
-      </div>
-    </div> -->
-
-    <!-- <div class="col-lg-5 col-md-4 col-sm-6 col-xs-12">
-      <img class="img-responsive" src="../assets/img/theeplogo.jpg" alt="" />
-    </div> -->
-    <div class="profilecontent">
-    <b-card no-body class="overflow-hidden">
-    <b-row no-gutters>
-      <b-col md="7">
-      <b-card-body title="">
-          <b-card-text>
-          Hi! My name is Pratheep Sinnappu. <br><br>I have Completed Computer Science and BA top-up in Web Development at
-          Erhvervsakademi og Professionshøjskole (UCL) in Odense.<br><br>
-          Right now, I am looking for Internship, full time job or part-time job to my relevant education. Further details, I have attached my CV in the skills Page.<br><br>
-Since I am recently graduated, I am ready to start-up with any kind of IT jobs to get the experience and grow in my carrier.
-
-          </b-card-text>
-        </b-card-body>
-      </b-col>
-      <b-col md="5">
-        <div class=""  v-animate-css.delay-20s="'bounceInRight'">
-        <img class="profileimg-responsive" src="../assets/img/theep-photo2.jpg" alt="" onContextMenu="return false;"/>
+  <div class="animated bounceInLeft delay-4s">
+    <div class="Profile">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 col-lg-6">
+            <p class="profile-text">
+              Hi! My name is Pratheep Sinnappu. <br /><br />I have Completed
+              Computer Science and BA top-up in Web Development at
+              Erhvervsakademi og Professionshøjskole (UCL) in Odense.<br /><br />
+              Right now, I am looking for Internship, full time job or part-time
+              job to my relevant education. Further details, I have attached my
+              CV in the skills Page.<br /><br />
+              Since I am recently graduated, I am ready to start-up with any
+              kind of IT jobs to get the experience and grow in my carrier.
+            </p>
+          </div>
+          <div class="col-12 col-lg-6" v-animate-css.delay-20s="'bounceInRight'">
+            <img
+              class="profile-img"
+              src="../assets/img/theep-photo2.jpg"
+              alt=""
+              onContextMenu="return false;"
+            />
+          </div>
         </div>
-    </b-col>
-    </b-row>
-  </b-card>
-  </div>
-  </div>
+      </div>
+    </div>
   </div>
 </template>
