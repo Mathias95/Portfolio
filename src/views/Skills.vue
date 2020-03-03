7b8ffd30b922d322cf518c381c755111fb036284
Mathias Jacobsen <mathiasrjacobsen@gmail.com>
2020-03-03 19:07:48 +0100

    small changes on skills section

--- src/views/Skills.vue
+++ src/views/Skills.vue
@@ -3,46 +3,50 @@
     <div class="container">
       <div class="row">
         <div class="col-12 col-md-10">
-          <div class="skills-wrapper">
-            <div class="skills-frontend">
-              <div class="skills-card">
-                <img src="../assets/img/skills/wordpress.png" alt="" />
-                <p class="skills-text">WORDPRESS</p>
-              </div>
-              <div class="skills-card">
-                <img src="../assets/img/skills/wordpress.png" alt="" />
-                <p class="skills-text">Javascript</p>
-              </div>
-              <div class="skills-card">
-                <img src="../assets/img/skills/wordpress.png" alt="" />
-                <p class="skills-text">Javascript</p>
-              </div>
-              <div class="skills-card">
-                <img src="../assets/img/skills/wordpress.png" alt="" />
-                <p class="skills-text">Javascript</p>
+          <div class="skills-wrapper row">
+            <div class="skills-frontend col-6">
+              <div class="row">
+                <div class="skills-card col-sm-6 col-lg-4">
+                  <img class="skills-icon" src="../assets/img/skills/wordpress.png" alt="" />
+                  <p class="skills-text">WORDPRESS</p>
+                </div>
+                <div class="skills-card col-sm-6 col-lg-4">
+                  <img class="skills-icon" src="../assets/img/skills/wordpress.png" alt="" />
+                  <p class="skills-text">Javascript</p>
+                </div>
+                <div class="skills-card col-sm-6 col-lg-4">
+                  <img class="skills-icon" src="../assets/img/skills/wordpress.png" alt="" />
+                  <p class="skills-text">Javascript</p>
+                </div>
+                <div class="skills-card col-sm-6 col-lg-4">
+                  <img class="skills-icon" src="../assets/img/skills/wordpress.png" alt="" />
+                  <p class="skills-text">Javascript</p>
+                </div>
               </div>
             </div>
-            <div class="skills-backend">
-              <div class="skills-card">
-                <img src="../assets/img/skills/wordpress.png" alt="" />
-                <p class="skills-text">WORDPRESS</p>
-              </div>
-              <div class="skills-card">
-                <img src="../assets/img/skills/wordpress.png" alt="" />
-                <p class="skills-text">Javascript</p>
-              </div>
-              <div class="skills-card">
-                <img src="../assets/img/skills/wordpress.png" alt="" />
-                <p class="skills-text">WORDPRESS</p>
-              </div>
-              <div class="skills-card">
-                <img src="../assets/img/skills/wordpress.png" alt="" />
-                <p class="skills-text">Javascript</p>
+            <div class="skills-backend col-6">
+              <div class="row">
+                <div class="skills-card col-sm-6 col-lg-4">
+                  <img class="skills-icon" src="../assets/img/skills/wordpress.png" alt="" />
+                  <p class="skills-text">WORDPRESS</p>
+                </div>
+                <div class="skills-card col-sm-6 col-lg-4">
+                  <img class="skills-icon" src="../assets/img/skills/wordpress.png" alt="" />
+                  <p class="skills-text">Javascript</p>
+                </div>
+                <div class="skills-card col-sm-6 col-lg-4">
+                  <img class="skills-icon" src="../assets/img/skills/wordpress.png" alt="" />
+                  <p class="skills-text">WORDPRESS</p>
+                </div>
+                <div class="skills-card col-sm-6 col-lg-4">
+                  <img class="skills-icon" src="../assets/img/skills/wordpress.png" alt="" />
+                  <p class="skills-text">Javascript</p>
+                </div>
               </div>
             </div>
           </div>
         </div>
-        <div class="col-12 col-md-2">
+        <div class="cv-wrapper col-12 col-md-2">
           <div id="cv" v-animate-css.hover="'flash'">
             <a
               href="https://drive.google.com/file/d/1ZqASnWeS5IfPXbFuIEf8-DuOqR2LtMvB/view?usp=sharing"
