8edd138a9321c02697b9c9488772d3adb4c31328
Mathias Jacobsen <mathiasrjacobsen@gmail.com>
2020-03-03 19:21:05 +0100

    fixed sidebar height

--- src/components/Pages.vue
+++ src/components/Pages.vue
@@ -1,34 +1,54 @@
 <template>
-<div>
-  <div id="maincontent" >
-  <div id="sidebar" >
-    <div class="section-slider" >
-    <div class="slider-nav animated zoomInDown delay-15s">
-        <router-link class="slider-link " to='#Profile' v-scroll-to="'#Profile'" title="Profile">01</router-link>
-        <router-link class="slider-link" to='#Skills' v-scroll-to="'#Skills'" title="Skills">02</router-link>
-        <router-link class="slider-link" to='#Projects' v-scroll-to="'#Projects'" title="Projects">03</router-link>
-        <router-link class="slider-link" to='#Contact' v-scroll-to="'#Contact'" title="Contact">04</router-link>
+  <div>
+    <div id="maincontent">
+      <div class="section-slider">
+        <div class="slider-nav animated zoomInDown delay-15s">
+          <router-link
+            class="slider-link "
+            to="#Profile"
+            v-scroll-to="'#Profile'"
+            title="Profile"
+            >01</router-link
+          >
+          <router-link
+            class="slider-link"
+            to="#Skills"
+            v-scroll-to="'#Skills'"
+            title="Skills"
+            >02</router-link
+          >
+          <router-link
+            class="slider-link"
+            to="#Projects"
+            v-scroll-to="'#Projects'"
+            title="Projects"
+            >03</router-link
+          >
+          <router-link
+            class="slider-link"
+            to="#Contact"
+            v-scroll-to="'#Contact'"
+            title="Contact"
+            >04</router-link
+          >
+        </div>
+      </div>
+      <div id="mainpage">
+        <div id="Profile" class="section">
+          <Profile />
+        </div>
+        <div id="Skills" class="section ">
+          <Skills />
+        </div>
+        <div id="Projects" class="section">
+          <Projects />
+        </div>
+        <div id="Contact" class="section">
+          <Contact />
+        </div>
+      </div>
     </div>
-    <!-- v-animate-css.click="'slideInRight'" -->
-</div>
-</div>
-<div id="mainpage" >
-<div id="Profile" class="section">
-      <Profile/>
-    </div>
-    <div id="Skills" class="section ">
-        <Skills/>
-    </div>
-    <div id="Projects" class="section">
-      <Projects/>
-    </div>
-    <div id="Contact" class="section" >
-      <Contact/>
-    </div>
-</div>
-</div>
-</div>
-
+  </div>
 </template>
 <script>
 import Contact from '@/views/Contact.vue'
